6857a441ccb129358711079d78ad7dd43a044ee8
Carlos Cima <carlos.cima@loggi.com>
2023-08-07 09:58:33 -0400

    Changed redis_client class property to protected

--- cache_tower/adapters/redis_adapter.py
+++ cache_tower/adapters/redis_adapter.py
@@ -6,29 +6,29 @@ import redis
 class RedisAdapter(BaseAdapter):
     def __init__(self, params: dict, namespace: str = "", ttl: int = 0) -> None:
         super().__init__(params, namespace, ttl)
-        self.redis_client = redis.Redis(**params)
+        self._redis_client = redis.Redis(**params)
 
     def get(self, key: str) -> Any:
-        return self.redis_client.get(self._create_key(key))
+        return self._redis_client.get(self._create_key(key))
 
     def mget(self, keys: List[str]) -> Dict[str, Optional[Any]]:
-        values = self.redis_client.mget(self._create_keys(keys))
+        values = self._redis_client.mget(self._create_keys(keys))
         return dict(zip(keys, values))
 
     def set(self, key: str, value: Any) -> None:
-        self.redis_client.set(self._create_key(key), value, ex=self._ttl)
+        self._redis_client.set(self._create_key(key), value, ex=self._ttl)
 
     def mset(self, items: Dict[str, Any]) -> None:
-        self.redis_client.mset(self._create_items(items))  # type: ignore
+        self._redis_client.mset(self._create_items(items))  # type: ignore
         if self._ttl > 0:
             for key in items:
-                self.redis_client.expire(self._create_key(key), self._ttl)
+                self._redis_client.expire(self._create_key(key), self._ttl)
 
     def delete(self, key: str) -> None:
-        self.redis_client.delete(self._create_key(key))
+        self._redis_client.delete(self._create_key(key))
 
     def exists(self, key: str) -> bool:
-        return bool(self.redis_client.exists(self._create_key(key)))
+        return bool(self._redis_client.exists(self._create_key(key)))
 
     def flush(self) -> None:
-        self.redis_client.flushdb()
+        self._redis_client.flushdb()
